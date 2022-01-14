f67c806e4330a95c1cd68404d0ed34206b45799f
“Deviarumsari30“ <deviarumsari30@gmail.com>
2022-01-15 02:47:09 +0700

    “Login“

--- index.php
+++ index.php
@@ -11,11 +11,14 @@
     font-family: 'Open Sans', sans-serif;
 }
 body{
-    height: 100vh;
-    background-image: url(https://beritaturki.com/wp-content/uploads/2021/02/Perpustakaan-Nasional-Turki.jpg);
+    margin: 0;
+    padding:0;
+    background-image: url(https://image.freepik.com/free-photo/pink-blue-leaves-pink-background-with-copyspace-top-side_24972-579.jpg);
     background-size: cover;
     background-position: center;
     background-repeat: no-repeat;
+    background-attachment: fixed;
+    font-family: sans-serif:
 }
 
 .container{
@@ -23,40 +26,42 @@ body{
     left: 50%;
     top: 50%;
     transform: translate(-50%,-50%);
-    padding: 20px 25px;
+    background: black(0, 0, 0, 0, 5);
+    padding: 20px;
     width: 300px;
+    box-shadow: 0 0 10px 5px black;
 
-    background-color: rgba(0,0,0,.7);
-    box-shadow: 0 0 10px rgba(255,255,255,.3);
+    background-color: black(0,0,0,.7);
+    box-shadow: 0 0 10px black(255,255,255,.3);
 }
 .container h1{
     text-align: left;
-    color: #fafafa;
+    color: black;
     margin-bottom: 30px;
-    text-transform: uppercase;
+    text-transform: black;
     border-bottom: 4px solid #2979ff;
 }
 .container label{
     text-align: center;
-    color: #90caf9;
+    color: black;
 }
 .container form input{
     width: calc(100% - 20px);
     padding: 8px 10px;
-    margin-bottom: 20px;
+    margin-bottom: 15px;
     border: none;
     background-color: transparent;
     border-bottom: 2px solid #2979ff;
-    color: #fff;
+    color: black;
     font-size: 20px;
 }
 .container form button{
     width: 100%;
     padding: 5px 0;
-    border: none;
+    border: black;
     background-color:#2979ff;
     font-size: 18px;
-    color: #fafafa;
+    color: black;
 }
 </style>
 
